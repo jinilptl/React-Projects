6935cd33dbc11ba717eb3b2c6f6db38a12c30e08
jinilptl <jrpatel1747@gmail.com>
2025-08-03 00:11:35 +0530

    favicon add and data change. all working

--- Docs Mini App/src/components/Foreground.jsx
+++ Docs Mini App/src/components/Foreground.jsx
@@ -14,34 +14,34 @@ const Foreground = () => {
     title: "Project Beta",
     semiTitle: "E-Commerce Dashboard",
     desc: "An advanced analytics dashboard for e-commerce admin. Features include sales stats, user engagement metrics, and product performance tracking.",
-    author: "Priya",
+    author: "Adarsh",
     timing: "1 week ago",
   },
   {
     title: "Project Gamma",
     semiTitle: "Real-time Chat App",
     desc: "Built using WebSocket and Node.js, this chat app enables real-time communication with typing indicators, emojis, and user presence.",
-    author: "Ravi",
+    author: "Krish",
     timing: "2 days ago",
   },
   {
     title: "Project Delta",
     semiTitle: "Portfolio Website",
     desc: "Personal portfolio built with React and Tailwind CSS. Includes smooth animations, project showcase, and contact form with validations.",
-    author: "Sneha",
+    author: "Parth",
     timing: "5 days ago",
   },
   {
     title: "Project Epsilon",
     semiTitle: "Health Tracker App",
     desc: "Tracks daily exercise, meals, and water intake. Integrated with health APIs and includes visualization of progress using charts.",
-    author: "Aman",
+    author: "Ajay",
     timing: "Yesterday",
   },
 ];
 
   let myref = useRef();
-  console.log('my ref is',myref);
+  
   
   return (
     <div
